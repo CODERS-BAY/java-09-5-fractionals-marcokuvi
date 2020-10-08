a4b5750562d5271c24c2b2db1c5accd49e773ca0
marco <kuvimarco@gmail.com>
2020-10-08 13:41:48 +0200

    All Tests are passed

---
+++ src/main/java/at/codersbay/datastructures/MathClass.java
@@ -0,0 +1,17 @@
+package at.codersbay.datastructures;
+
+public class MathClass {
+    public static Integer searchForGreatestCommonDivisor(Integer firstInteger, Integer secondInteger) {
+        if (firstInteger < 0)
+            firstInteger = -1 * firstInteger;
+        if (secondInteger < 0)
+            secondInteger = -1 * secondInteger;
+        Integer thirdInteger;
+        while (secondInteger != 0) {
+            thirdInteger = secondInteger;
+            secondInteger = firstInteger % secondInteger;
+            firstInteger = thirdInteger;
+        }
+        return firstInteger;
+    }
+}
